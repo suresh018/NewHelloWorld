6d9a60fd5d2f0f8ab684bddc933f00840931b820
suresh018 <suresh.cet018@gmail.com>
2018-03-12 13:05:01 +0530

    Hello World1

---
+++ helloworld.java
@@ -0,0 +1,14 @@
+public class helloworld{
+
+
+	public static void main(String args[]){
+
+		for(int i=1;i<=10;i++){
+			System.out.println("Hello World :"+i);
+		}
+		System.out.println("This is my first Git commit.");
+
+	}
+
+
+}
